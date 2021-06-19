0ba6e3fdc4c8ce7dbcd064ff92153537fb39748f
Nemor89 <express-don@mail.ru>
2021-06-19 15:35:52 +0300

    реализована страница редактирования категорий

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Blog\Admin\BaseController;
 
 class CategoryController extends BaseController
 {
@@ -14,7 +14,9 @@ class CategoryController extends BaseController
      */
     public function index()
     {
-        dd(__METHOD__);
+        $paginator = BlogCategory::paginate(5);
+
+        return view('blog.admin.categories.index', compact('paginator'));
     }
 
     /**
@@ -46,7 +48,10 @@ class CategoryController extends BaseController
      */
     public function edit($id)
     {
-        dd(__METHOD__);
+        $item = BlogCategory::findOrFail($id);
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
     /**
@@ -58,6 +63,22 @@ class CategoryController extends BaseController
      */
     public function update(Request $request, $id)
     {
-        dd(__METHOD__);
+        $item = BlogCategory::find($id);
+        if (empty($item))
+        {
+            return back()->withErrors(['msg' => "Запись с ID - $id не найдена"])->withInput();
+        }
+
+        $data = $request->all();
+        $result = $item->fill($data)->save();
+
+        if ($result)
+        {
+            return redirect()->route('blog.admin.categories.edit', $item->id)->with(['success' => 'Успешно сохранено']);
+        }
+        else
+        {
+            return back()->withErrors(['msg' => "Ошибка сохранения"])->withInput();
+        }
     }
 }
